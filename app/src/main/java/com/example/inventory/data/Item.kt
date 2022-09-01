a3fa8f4240b30990816de45c2fb83a4db74f5095
Sourabh Kumar <20010001030@gateway.edu.in>
2022-09-01 22:09:55 +0530

    Added price formatter

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -3,6 +3,7 @@ package com.example.inventory.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
 //Represent database entity of the app
 
@@ -20,3 +21,7 @@ data class Item(
     @ColumnInfo(name = "quantity")
     val quantityInStock: Int
 )
+
+//Format the item price to currency price format
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(itemPrice)
